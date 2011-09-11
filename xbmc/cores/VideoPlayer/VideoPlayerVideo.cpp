b887b621d94b27cd2916064795f04f5126b3a1b1
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2011-09-11 11:06:31 +0200

    Add display of current maximum bitrate in the 'CodecInfo' overlay window.
    Fixed the max bit rate value reset at the end of play.
    Enlarged the size of the Confluence skin codec information window to accomodate the new information.
    Added a log at the end of play printing some information about codec dropped frames behavior.

--- xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayerVideo.cpp
@@ -776,6 +776,11 @@ void CVideoPlayerVideo::Process()
 
   // we need to let decoder release any picture retained resources.
   m_pVideoCodec->ClearPicture(&picture);
+ //log max bitrate obtained
+  CLog::Log(LOGNOTICE, "%s.%s:%d - Max bitrate: %2.2f Mb/s Dropped %d",
+             __FILE__, __FUNCTION__, __LINE__,
+            m_videoStats.GetMaxBitrate() / (1024.0*1024.0), m_iDroppedFrames);
+  m_videoStats.ResetMaxBitrate();
 }
 
 void CVideoPlayerVideo::OnExit()
@@ -1089,7 +1094,8 @@ std::string CVideoPlayerVideo::GetPlayerInfo()
   s << "fr:"     << std::fixed << std::setprecision(3) << m_fFrameRate;
   s << ", vq:"   << std::setw(2) << std::min(99,GetLevel()) << "%";
   s << ", dc:"   << m_codecname;
-  s << ", Mb/s:" << std::fixed << std::setprecision(2) << (double)GetVideoBitrate() / (1024.0*1024.0);
+  s << ", Mb/s:" << std::setw(2) << std::fixed << std::setprecision(2) << (double)GetVideoBitrate() / (1024.0*1024.0);
+  s << "<"       << std::setw(5) << std::fixed << std::setprecision(2) << m_videoStats.GetMaxBitrate() / (1024.0*1024.0);
   s << ", drop:" << m_iDroppedFrames;
   s << ", skip:" << m_renderManager.GetSkippedFrames();
 
