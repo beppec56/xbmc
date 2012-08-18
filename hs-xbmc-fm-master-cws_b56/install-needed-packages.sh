d8ca5eb2476a8c6e43db8d8edc6eb37dfb135bfd
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2012-08-18 18:31:19 +0200

    Added necessary files to build xbmc in my way.
    Modified compile script file for 8 processors
    Modified script used to get my repo.
    Added new packages needed to build.
    Added the helper file a new package needed to build xbmc.

---
+++ hs-xbmc-fm-master-cws_b56/install-needed-packages.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+#sudo apt-get install git-core make g++ gcc gawk pmount libtool nasm yasm automake cmake gperf zip unzip bison libsdl-dev libsdl-image1.2-dev libsdl-gfx1.2-dev libsdl-mixer1.2-dev libfribidi-dev liblzo2-dev libfreetype6-dev libsqlite3-dev libogg-dev libasound2-dev python-sqlite libglew-dev libcurl3 libcurl4-gnutls-dev libxrandr-dev libxrender-dev libmad0-dev libogg-dev libvorbisenc2 libsmbclient-dev libmysqlclient-dev libpcre3-dev libdbus-1-dev libhal-dev libhal-storage-dev libjasper-dev libfontconfig-dev libbz2-dev libboost-dev libenca-dev libxt-dev libxmu-dev libpng-dev libjpeg-dev libpulse-dev mesa-utils libcdio-dev libsamplerate-dev libmpeg3-dev libflac-dev libiso9660-dev libass-dev libssl-dev fp-compiler gdc libmpeg2-4-dev libmicrohttpd-dev libmodplug-dev libssh-dev gettext cvs python-dev libyajl-dev libboost-thread-dev libplist-dev autopoint
+
+#questa è per xubuntu 11.10
+#necessario prima compilare la versione di libcrystalhd, in sorgente (a xbmc-debian-src/libcrystalhd)
+#sudo apt-get install  debhelper python-support cmake \
+# autotools-dev autoconf automake unzip libboost-dev zip libtool \
+# libgl1-mesa-dev libglu1-mesa-dev libglew1.6-dev \
+# libmad0-dev libjpeg62-dev libsamplerate0-dev libogg-dev libvorbis-dev \
+# libfreetype6-dev libfontconfig1-dev libbz2-dev libfribidi-dev \
+# libsqlite3-dev libmysqlclient-dev libasound2-dev libpng12-dev \
+# libpcre3-dev liblzo2-dev libcdio-dev libsdl1.2-dev libsdl-image1.2-dev \
+# libsdl-mixer1.2-dev libenca-dev libjasper-dev libxt-dev \
+# libxmu-dev libxinerama-dev libcurl4-gnutls-dev  \
+# libdbus-1-dev libhal-storage-dev libhal-dev libpulse-dev  \
+# libavahi-common-dev libavahi-client-dev libxrandr-dev libavcodec-dev \
+# libavformat-dev libavutil-dev libpostproc-dev libswscale-dev \
+# libmpeg2-4-dev libass-dev libmpcdec-dev libflac-dev \
+# python-dev gawk gperf nasm libcwiid-dev \
+# libbluetooth-dev zlib1g-dev libsmbclient-dev libboost-thread-dev \
+# libiso9660-dev libssl-dev lsb-release \
+# libvdpau-dev libvdpau-doc \
+# libmicrohttpd-dev \
+# libmodplug-dev librtmp-dev curl python-dev libyajl-dev \
+# libplist-dev libtinyxml-dev
+
+#questa è per xubuntu 12.04
+sudo apt-get install autoconf automake autopoint autotools-dev cmake curl \
+  debhelper gawk gperf libao-dev libasound2-dev \
+  libass-dev libavahi-client-dev libavahi-common-dev \
+  libavcodec-dev libavfilter-dev libavformat-dev libavutil-dev \
+  libbluetooth-dev libbluray-dev libboost-dev libboost-thread-dev \
+  libiso9660-dev libbz2-dev libcdio-dev libcec-dev libcrystalhd-dev  \
+  libcurl4-gnutls-dev libcwiid-dev libdbus-1-dev \
+  libenca-dev libflac-dev libfontconfig-dev libfreetype6-dev \
+  libfribidi-dev libgl1-mesa-dev libglew-dev \
+  libglu1-mesa-dev libhal-dev libhal-storage-dev \
+  libjasper-dev libjpeg-dev libltdl-dev liblzo2-dev libmad0-dev \
+  libmicrohttpd-dev libmodplug-dev libmpcdec-dev libmpeg2-4-dev \
+  libmysqlclient-dev libnfs-dev libogg-dev libpcre3-dev libplist-dev \
+  libpng12-dev libpostproc-dev libpulse-dev librtmp-dev \
+  libsamplerate-dev libsdl-dev libsdl-image1.2-dev libsdl-mixer1.2-dev \
+  libshairport-dev libsmbclient-dev libsqlite3-dev libssl-dev \
+  libswscale-dev libtinyxml-dev libtool libudev-dev libusb-dev libva-dev \
+  libvdpau-dev libvorbis-dev libxinerama-dev libxmu-dev libxrandr-dev \
+  libxt-dev libyajl-dev lsb-release nasm python-dev \
+  python-support unzip yasm zip zlib1g-dev libcap-dev \
+  libglew-dev swig openjdk-6-jdk mesa-utils \
+  libssh-dev
