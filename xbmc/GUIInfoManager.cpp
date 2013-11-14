3f8137d078632c3fd151208728268de63b8fa794
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2013-11-14 14:25:38 +0100

    Fix after b85b12f8cc0855e7d4d448886a4d60adeba7ce6e (kill CStdString)

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -1712,7 +1712,7 @@ CStdString CGUIInfoManager::GetLabel(int info, int contextWindow, std::string *f
     break;
   case SYSTEM_XBMC_CORE_USAGE:
 #if defined(_LINUX)
-    strLabel.Format("(CPU-XBMC %4.2f%%)", m_resourceCounter.GetCPUUsage());
+    strLabel = StringUtils::Format("(CPU-XBMC %4.2f%%)", m_resourceCounter.GetCPUUsage());
 #endif
     return strLabel;
     break;
@@ -4223,7 +4223,7 @@ int CGUIInfoManager::GetCPUFanSpeed()
   char        scale = 0;
   FILE        *p    = NULL;
 
-  if (cmd.IsEmpty() || !(p = popen(cmd.c_str(), "r")))
+  if (cmd.empty() || !(p = popen(cmd.c_str(), "r")))
     return -2; // no command
 
   ret = fscanf(p, "%d", &value);
